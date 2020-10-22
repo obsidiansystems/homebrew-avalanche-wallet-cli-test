b54d09d661cf860a45103abae4fe2a8bf1b6f8c8
Nate Homesley <nehomesley@gmail.com>
2020-10-22 16:23:32 -0400

    Update avalanche-wallet-cli-develop.rb

--- Formula/avalanche-wallet-cli-develop.rb
+++ Formula/avalanche-wallet-cli-develop.rb
@@ -6,7 +6,7 @@ class AvalancheWalletCliDevelop < Formula
   desc ""
   homepage ""
   license ""
-  head "https://github.com/obsidiansystems/avalanche-wallet-cli.git/tree/develop"
+  head "https://github.com/obsidiansystems/avalanche-wallet-cli.git/develop"
 
   depends_on "yarn" => :build
   depends_on "nvm" => :build
